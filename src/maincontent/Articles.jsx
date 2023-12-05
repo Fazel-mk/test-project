119a6c0c18541ae18b27511040f5bb69e9e99ab0
Fazel-mk <125917840+Fazel-mk@users.noreply.github.com>
2023-12-05 10:21:52 +0330

    articles are added but the css is not complete yet

--- src/maincontent/Articles.jsx
+++ src/maincontent/Articles.jsx
@@ -1,20 +1,57 @@
 import React from "react";
+import "./Articles.css";
 
 const Articles = () => {
   return (
     <div className="Articles">
       <div className="articleTitle">
-        <span className="title"></span>
+        <span className="title">آخرین مقالات</span>
       </div>
       <div className="articleCards">
-        <div className="articleWrapper1">
-          <div className="card1">
-            <div className="cardText"></div>
-            <div className="cardPic"></div>
+        <div className="card1">
+          <div className="cardText">
+            <div className="title1">نحوه کار با اهن</div>
+            <p className="cardDetails">
+              مرکز آهن با نام تجاری آهن سجاد مهر سپاهان فعالیت خود را در زمینه
+              خرید فروش آهن آلات با ۳۰ سال سابقه و با تلاش بی وقفه و سرلوحه قرار
+              دادن شعار صداقت در عملکرد و خدمت به مشتریان آغاز کرده. تلاش میشود
+              همگام با گسترش و
+            </p>
           </div>
-          <div className="card2"></div>
+          <div className="cardPic">
+            <img src="./image2.png" alt="" />
+          </div>
         </div>
-        <div className="articleWrapper2"></div>
+
+        <div className="card2">
+          <div className="cardText">
+            <div className="title1">نحوه کار با اهن</div>
+            <p className="cardDetails">
+              مرکز آهن با نام تجاری آهن سجاد مهر سپاهان فعالیت خود را در زمینه
+              خرید فروش آهن آلات با ۳۰ سال سابقه و با تلاش بی وقفه و سرلوحه قرار
+              دادن شعار صداقت در عملکرد و خدمت به مشتریان آغاز کرده. تلاش میشود
+              همگام با گسترش و
+            </p>
+          </div>
+          <div className="cardPic">
+            <img src="./image2.png" alt="" />
+          </div>
+        </div>
+
+        <div className="card3">
+          <div className="cardText">
+            <div className="title1">نحوه کار با اهن</div>
+            <p className="cardDetails">
+              مرکز آهن با نام تجاری آهن سجاد مهر سپاهان فعالیت خود را در زمینه
+              خرید فروش آهن آلات با ۳۰ سال سابقه و با تلاش بی وقفه و سرلوحه قرار
+              دادن شعار صداقت در عملکرد و خدمت به مشتریان آغاز کرده. تلاش میشود
+              همگام با گسترش و
+            </p>
+          </div>
+          <div className="cardPic">
+            <img src="./image2.png" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
