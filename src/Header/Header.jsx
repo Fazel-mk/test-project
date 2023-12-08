b1a621f6ae10fb75cc9b1404925c863c7690b702
Fazel-mk <125917840+Fazel-mk@users.noreply.github.com>
2023-12-08 16:00:37 +0330

    header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import "./Header.css";
 import BreakingNews from "./BreakingNews";
 import TopNav from "./TopNav";
 import SearchBar from "./SearchBar";
 import Navbar from "./Navbar";
+import "./Header.css";
+
 const Header = () => {
   return (
     <header>
